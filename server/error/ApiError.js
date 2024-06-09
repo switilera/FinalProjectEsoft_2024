ac1916ba8097c619be90dd6e478750c438495612
Lera Donets <lera72tymen@gmail.com>
2024-06-09 14:43:20 +1000

    Backend

---
+++ server/error/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+  static badRequest(message) {
+    return new ApiError(400, message);
+  }
+  static Unauthorized(message) {
+    return new ApiError(401, message);
+  }
+  static forbidden(message) {
+    return new ApiError(403, message);
+  }
+  static NotFound(message) {
+    return new ApiError(404, message);
+  }
+  static Inrernal(message) {
+    return new ApiError(500, message);
+  }
+}
+
+module.exports = ApiError;
