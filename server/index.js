a5b4af77ed2b44b332b2949c8cd626b0d17c0722
Lera Donets <lera72tymen@gmail.com>
2024-06-11 01:31:18 +1000

    Backend

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const sequelize = require("./bd");
 const port = process.env.port || 5000;
 const moduls = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use(`/api`, router);
 
 // Обработка ошибок
--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -4,5 +4,8 @@ module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message });
   }
-  return res.status(500).json({ message: "Непредвиденная ошибка!" });
+  return res.status(500).json({
+    message:
+      "На сервере произошла ошибка, в результате которой он не может успешно обработать запрос",
+  });
 };
