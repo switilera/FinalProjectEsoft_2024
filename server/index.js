de20e7414d90bd9c4065a56d25fa6667e644db52
Lera Donets <lera72tymen@gmail.com>
2024-06-10 18:56:37 +1000

    Backend

--- server/index.js
+++ server/index.js
@@ -5,15 +5,11 @@ const port = process.env.port || 5000;
 const moduls = require("./models/models");
 const cors = require("cors");
 const router = require("./routes/index");
-const errorHadler = require("./middleware/ErrorHandlingMiddleware");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(`/api`, router);
-
-//Обработка ошибок
-app.use(errorHadler);
 
 /*app.get("/", (req, res) => {
   res.status(200).json({ message: "Работаем" });
