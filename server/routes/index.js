3d69254421082693ec081b8cfca590044bd0db5a
Lera Donets <lera72tymen@gmail.com>
2024-06-13 19:46:06 +1000

    Backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,11 @@ const deviceRouter = require("./deviceRouter");
 const userRouter = require("./userRouter");
 const brandRouter = require("./brandRouter");
 const typeRouter = require("./typeRouter");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.use(`/user`, userRouter);
-router.use(`/type`, typeRouter);
-router.use(`/brand`, brandRouter);
-router.use(`/device`, deviceRouter);
+router.use(`/type`, checkRole("ADMIN"), typeRouter);
+router.use(`/brand`, checkRole("ADMIN"), brandRouter);
+router.use(`/device`, checkRole("ADMIN"), deviceRouter);
 
 module.exports = router;
