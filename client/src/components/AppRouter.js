b654cf65b2128a69f9de91bf67384b04577e7ac5
Lera Donets <lera72tymen@gmail.com>
2024-06-17 21:49:51 +1000

    Frontend

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Switch, Route, Redreact } from "react-router-dom";
+
+const AppRouter = () => {
+  return <div></div>;
+};
+
+export default AppRouter;
---
+++ client/src/routes.js
@@ -0,0 +1,3 @@
+export const authRoutes = [{}];
+
+export const publicRoutes = [{}];
